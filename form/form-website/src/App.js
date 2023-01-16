d889f8dc9a540c7b0d86dd771a1ad4237a5d736c
odgra003 <odgra003@osloskolen.no>
2023-01-16 12:09:01 +0100

    made form

--- form/form-website/src/App.js
+++ form/form-website/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Søknad</h1>
+      <form id='applicationForm'>
+        <label>Fornavn:</label>
+        <input type="text" placeholder='Fornavn'></input>
+        <label>Etternavn:</label>
+        <input type="text" placeholder='Etternavn'></input>
+        <label>E-post:</label>
+        <input type="text" placeholder='E-post'></input>
+        <label>Telefonnummer:</label>
+        <input type="text" placeholder='Tlf'></input>
+        <label>Skole:</label>
+        <input type="text" placeholder='Skole'></input>
+        <input type="submit"></input>
+      </form>
     </div>
   );
 }

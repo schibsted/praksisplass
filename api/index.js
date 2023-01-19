79f3fd7846d920f21e8f391f6fb1a148ae6941a3
odgra003 <odgra003@osloskolen.no>
2023-01-19 12:04:26 +0100

    uploads files to s3 and saves the filename in the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 require('dotenv').config({})
+const express = require('express')
+const cors = require('cors');
+const S3Client = require('@aws-sdk/client-s3').S3Client
+const AbortMultipartUploadCommand = require('@aws-sdk/client-s3').AbortMultipartUploadCommand
+const fs = require('fs')
+const bodyParser = require('body-parser');
+const { ListObjectsCommand, PutObjectCommand, AbortMultipartUploadOutputFilterSensitiveLog } = require('@aws-sdk/client-s3');
+const app = express()
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
 
 var mysql = require('mysql');
 
@@ -10,15 +21,7 @@ var con = mysql.createConnection({
   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
 });
 
-const express = require('express')
-const cors = require('cors');
-const S3Client = require('@aws-sdk/client-s3').S3Client
-const AbortMultipartUploadCommand = require('@aws-sdk/client-s3').AbortMultipartUploadCommand
-const fs = require('fs')
-const multer = require('multer')
-const bodyParser = require('body-parser');
-const { RestoreObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
-const app = express()
+
 
 app.use(cors());
 
@@ -28,56 +31,90 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send(JSON.stringify({
         name: 'test'
     }))
 })
 
-app.post('/sendData', (req, res) => {
-    console.log(req.body)
-    let data = req.body
-    con.connect(function(err) {
-        var sql = "INSERT INTO applications (firstname, lastname, email, phoneNumber, school, study) VALUES ?";
-        var values = [[data.firstname, data.lastname, data.email, data.phoneNumber, data.school, data.study]]
-        con.query(sql, [values], function (err, result) {
-            if (err) throw err;
-            console.log("1 record inserted");
-        });
-    });
-
-    
-})
-
-app.post('/uploadFile', (req, res) => {
-    console.log(req.body)
-})
-
-app.post('/upload', async function (req, res) {
-    const file = req.body.applicationLetter
-    
+app.get('/listBucketData', async (req, res) => {
     const client = new S3Client({
         region: 'eu-north-1',
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     });
 
-    const blob = fs.readFileSync(file);
+    const command = new ListObjectsCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Delimiter: '/',
+    })
+
+    const objects = await client.send(command);
+    res.send(objects.Contents.map(file => file.Key).join('<br/>'))
+})
+
+// uploads data filled out in in the form
+app.post('/upload', upload.array('files'), (req, res) => {
+    uploadToDatabase(req.body, req.files)
+
+    req.files.forEach(file => {
+        const [fileName, filePath, fileType] = [file.filename, file.path, file.mimetype]
+        thowFilesInBucket(fileName, filePath, fileType)
+        fs.unlink(file.path, resultHandler);
+    });
+});
+
+let resultHandler = function (err) {
+    if (err) {
+        console.log("unlink failed", err);
+    } else {
+        console.log("file deleted");
+    }
+}
+
+// sends data to the database
+const uploadToDatabase = async (data, files) => {
+    con.connect(function(err) {
+        var sql = "INSERT INTO Applications (firstname, lastname, email, phoneNumber, school, study) VALUES ?";
+        var values = [[data.firstname, data.lastname, data.email, data.phoneNumber, data.school, data.study]]
+        con.query(sql, [values], function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+        });
+
+        files.forEach(file => {
+            var sql = "INSERT INTO Files (filename, application) VALUES (?, (SELECT (id) FROM Applications WHERE id=(SELECT max(id) FROM Applications)))"
+            var values = [[file.filename]]
+    
+            con.query(sql, [values], function (err, result) {
+                if (err) throw err;
+                console.log("1 file inserted");
+            });
+        });
+    });    
+};
+
+// uploads files to s3
+async function thowFilesInBucket(fileName, filePath, fileType) {    
+    const client = new S3Client({
+        region: 'eu-north-1',
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    });
+
+    const blob = fs.readFileSync(filePath);
 
     const command = new PutObjectCommand({
         Bucket: process.env.BUCKET_NAME,
-        Key: asdfsfasd.txt,
+        Key: fileName,
         Body: blob,
+        ContentType: fileType,
     })
 
     const uploadImage = await client.send(command);
-
-    res.send({
-        Location: uploadImage.Location || 'undefined',
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        Bucket: process.env.BUCKET_NAME,
-    })
-});
+};
 
 app.listen(process.env.PORT || 3100, () => console.log('server is running'))
--- form/form-website/src/App.js
+++ form/form-website/src/App.js
@@ -15,33 +15,26 @@ function App() {
 
     const formData = new FormData()
 
-    formData.append("firstame", firstname)
+    formData.append("firstname", firstname)
     formData.append("lastname", lastname)
     formData.append("email", email)
     formData.append("phoneNumber", phoneNumber)
     formData.append("school", school)
     formData.append("study", study)
-    formData.append("applicationletter", applicationLetter)
     
-    console.log(formData)
-    // const data = {
-    //   firstname: firstname,
-    //   lastname: lastname,
-    //   email: email,
-    //   phoneNumber: phoneNumber,
-    //   school: school,
-    //   study: study,
-    // }
+    for(let i =0; i < applicationLetter.length; i++) {
+      formData.append("files", applicationLetter[i]);
+}
 
     const fetchAPI = async (data) => {
       const request = await fetch(`http://localhost:3100/upload`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'multipart/form-data'
+          // 'Content-Type': 'multipart/form-data'
           // 'Content-Type': 'application/json'
           // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: JSON.stringify(data)
+        body: formData,
       })
 
       const promise = await request.json()
@@ -49,7 +42,7 @@ function App() {
       return promise
     } 
 
-    fetchAPI(data).then(res => {
+    fetchAPI(formData).then(res => {
       console.log(res.name)
     })
   }  
@@ -57,22 +50,22 @@ function App() {
   return (
     <div className="App">
       <h1>Søknad</h1>
-      <form action='#' id='applicationForm'>
+      <form action="#" id="applicationForm">
         <label>Fornavn:</label>
-        <input type="text" placeholder='Fornavn' onChange={e => setFirstname(e.target.value)}></input>
+        <input type="text" placeholder="Fornavn" onChange={e => setFirstname(e.target.value)} />
         <label>Etternavn:</label>
-        <input type="text" placeholder='Etternavn' onChange={e => setLastname(e.target.value)}></input>
+        <input type="text" placeholder="Etternavn" onChange={e => setLastname(e.target.value)} />
         <label>E-post:</label>
-        <input type="text" placeholder='E-post' onChange={e => setEmail(e.target.value)}></input>
+        <input type="email" placeholder="E-post" onChange={e => setEmail(e.target.value)} />
         <label>Telefonnummer:</label>
-        <input type="text" placeholder='Tlf' onChange={e => setPhoneNumber(e.target.value)}></input>
+        <input type="tel" placeholder="Tlf" onChange={e => setPhoneNumber(e.target.value)} />
         <label>Skole:</label>
-        <input type="text" placeholder='Skole' onChange={e => setSchool(e.target.value)}></input>
+        <input type="text" placeholder="Skole" onChange={e => setSchool(e.target.value)} />
         <label>Linje:</label>
-        <input type="text" placeholder='Linje' onChange={e => setStudy(e.target.value)}></input>
-        <label>Søknad(pdf):</label>
-        <input type="file" onChange={e => setApplicationLetter(e)} />
-        <input type="submit" onClick={e => handleSubmit(e)}></input>
+        <input type="text" placeholder="Linje" onChange={e => setStudy(e.target.value)} />
+        <label>Filer(pdf):</label>
+        <input type="file"  multiple accept="application/pdf" onChange={e => setApplicationLetter(e.target.files)} />
+        <input type="submit" onClick={e => handleSubmit(e)} />
       </form>
     </div>
   );
